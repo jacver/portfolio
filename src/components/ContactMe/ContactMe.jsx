966a15d7a6b56ffe1a4512031c6762a848f700e0
Jacob Vernau <jacobvernau@gmail.com>
2022-06-30 12:12:10 -0600

    contact form completed

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -5,12 +5,15 @@ function ContactMe() {
   return (
     <div className="contact-container">
       <h3 className="text-warning">Let's Chat!</h3>
-      <button type="button" className="btn btn-outline-warning">
-        <span>
-          <i id="buttonID" className="far fa-envelope-open button-icon"></i>
-        </span>
-        <span className="btn-text">Contact</span>
-      </button>
+      <p>Please don't hesitate to reach out for anything!</p>
+      <a href="mailto:jacobvernau@gmail.com">
+        <button type="button" className="btn btn-outline-warning">
+          <span>
+            <i id="buttonID" className="far fa-envelope-open button-icon"></i>
+          </span>
+          <span className="btn-text">Contact</span>
+        </button>
+      </a>
     </div>
   );
 }
