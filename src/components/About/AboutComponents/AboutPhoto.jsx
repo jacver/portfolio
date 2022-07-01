f5176d95c38769e3a4c90d19c3f08c21b4447a9e
Jacob Vernau <jacobvernau@gmail.com>
2022-07-01 15:56:56 -0600

    images rendering on localhost

--- src/components/About/AboutComponents/AboutPhoto.jsx
+++ src/components/About/AboutComponents/AboutPhoto.jsx
@@ -1,12 +1,16 @@
 // src/components/About/AboutComponents/AboutPhoto.jsx
 import React from "react";
 import "../about.css";
-import portrait from "../../../images/about/portrait.jpeg";
+// import portrait from "../../../images/about/portrait.jpeg";
 
 function AboutPhoto() {
   return (
     <div className="portrait-container">
-      <img className="portrait" src={portrait} alt="portrait" />
+      <img
+        className="portrait"
+        src="https://i.imgur.com/qzxaW70.jpg"
+        alt="portrait"
+      />
     </div>
   );
 }
